ab928efd0521ffc0a3bf970dfb235cad58b37941
Алексей Лукин <34923601+DeadScreams@users.noreply.github.com>
2022-12-24 19:38:33 +0300

    Update config.js

--- config.js
+++ config.js
@@ -26,7 +26,7 @@ const CONFIG = {
 	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
 
 	// Weather
-	weatherKey: '4e71d6e59856b13c4d47e7824863ddbe', // Write here your API Key
+	weatherKey: 'cd865def6475b061bb5edbf58c60b4f1"', // Write here your API Key
 	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
 	weatherUnit: 'C', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
